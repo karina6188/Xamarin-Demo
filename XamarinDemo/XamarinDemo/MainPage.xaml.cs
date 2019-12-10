9174759d3c2dbc992b6144b77de95c972def5916
Karina Chen <karina6188@gmail.com>
2019-12-10 15:27:51 -0800

    Add project files.

---
+++ XamarinDemo/XamarinDemo/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinDemo
+{
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            button.Clicked += Button_Clicked;
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            string name = entry.Text;
+            await DisplayAlert($"Hi there", $"{name}!", "welcome");
+        }
+    }
+}
